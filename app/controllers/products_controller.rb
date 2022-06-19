125e98917d4ce45e07c4f0621e2e7e31e53a4f17
Viktor Tagil <viktor.tagil@gmail.com>
2022-06-19 13:49:47 +0200

    product crud actions working

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,46 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to warehouses_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path, status: :see_other
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :warehouse_id)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "creating seed"
-testwarehouse = Warehouse.create(name: "testwarehouse", location: "London")
-puts "seed created"
+puts "Destroying Warehouses"
+Warehouse.destroy_all
+
+puts "creating warehouses"
+
+Warehouse.create(name:'Supermall', location: 'London')
+Warehouse.create(name:'Illum', location: 'London')
+
+kaufland = Warehouse.create(name:'Kaufland', location: 'Birmingham')
+
+puts "Adding products for Kaufland"
+Product.create(name:'Bobbycar', price: '20', description: 'Kids toy', warehouse: kaufland)
+Product.create(name:'Teddy Bear', price: '10', description: 'Kids toy', warehouse: kaufland)
+
+puts "Warehouses and prodcuts added"
